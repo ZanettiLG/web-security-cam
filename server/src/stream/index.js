97f16e3b2aa821c890d988097aae68e0ea2f5840
Luis Gustavo Zanetti <luisgustavoczp@gmail.com>
2024-06-01 14:48:49 -0300

    reestructuring

--- server/src/stream/index.js
+++ server/src/stream/index.js
@@ -1,5 +1,8 @@
 const rtsp = require('rtsp-relay');
+const RtspServer = require('node-rtsp-stream');
+const {uuid} = require('../utils');
 const { connect } = require("../onvif/connect");
+const records = require("./records");
 
 async function provideStream (app, ip, {user="admin", pass="admin", port="554"})
 {
@@ -8,8 +11,29 @@ async function provideStream (app, ip, {user="admin", pass="admin", port="554"})
   const url = await connect (ip, {user, pass});
   console.log("ONVIF", url);
 
+  const filePath = "../records/" + uuid() + ".mp4";
+  const videoUrl = `rtsp://${user}:${pass}@${ip}:${port}/onvif1`;
+  
+  /* // Configurações do servidor RTSP
+  const rtspServer = new RtspServer({
+    name: 'stream',
+    cameras: {
+      cam1: {
+        url: url, // URL do stream RTSP
+        ffmpeg: '/usr/bin/ffmpeg', // Caminho para o binário ffmpeg
+        resolution: '640x480', // Resolução do vídeo
+        quality: 3, // Qualidade do vídeo (0 a 31)
+        // Outras opções ffmpeg podem ser especificadas aqui
+      },
+    },
+  }); */
+
+  /* rtspServer.start(); */
+
+  records.start(url, filePath);
+
   const handler = proxy({
-    url: `rtsp://${user}:${pass}@${ip}:${port}/onvif1`,//onvif1
+    url: videoUrl,//onvif1
     // if your RTSP stream need credentials, include them in the URL as above
     verbose: true,
   });
